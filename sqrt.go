02722f3446227d68ad93fe6c6088c15f171da4f4
Michael Joshua <michaeljoshua55555@gmail.com>
2019-11-30 19:09:23 +0530

    5lakh sqrt

--- sqrt.go
+++ sqrt.go
@@ -6,5 +6,28 @@ import (
 )
 
 func main() {
-	fmt.Println(math.Sqrt(300000))
+	fmt.Println(math.Sqrt(500000))
 }
+
+
+
+// 1. Dev --- Unit Testing 
+// 2. Test 
+// 3. UAT
+// 4. Production 
+
+
+
+// Golan backend 90
+// front end 
+
+// version - 20 test cases - 
+// version 2 - 21 ---- CRUD superagent 
+
+
+// 1. apis (katal) Job1
+// 2. webui testcases 
+// 3. mobile testcases 
+
+
+
